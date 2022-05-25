94d598e1b391f808781986f9265250b1b1600002
jefflangtech <jeff.m.lang@gmail.com>
2022-05-24 20:58:32 -0700

    Added app.js to repo and the readme

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,25 @@
-fetch('./data.json')
-  .then(response => {
-    return response.json()
+// fetch('./data.json')
+//   .then(response => {
+//     return response.json()
+//   })
+//   .then(data => console.log(data))
+
+async function getExpenseData() {
+  const response = await fetch('./data.json')
+  const data = await response.json()
+  displayData(data)
+}
+
+const displayData = function(data) {
+  data.forEach(element => {
+    console.log(element)
   })
-  .then(jsondata => console.log(jsondata))
+}
+
+// getExpenseData()
+
+const dataContainer = document.querySelector('.data-container')
+const para = document.createElement('p')
+para.innerText = 'test text'
+dataContainer.appendChild(para)
+console.dir(para)
